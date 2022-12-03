f04a8716d9af28587525ba49d3d685e3855352f7
ionutrogojan <i.rogojan15@gmail.com>
2022-12-03 20:06:18 +0000

    plugin test

--- plugin.js
+++ plugin.js
@@ -2,6 +2,6 @@
 import init, { add } from "./pkg/wasm_test.js";
 init().then(() => {
     const element = document.createElement("p");
-    element.textContent = `The plugin1 uses the add API to do: 1 + 1 = ${add(1, 1)}`
+    element.textContent = `The online plugin uses the add API to do: 3 + 3 = ${add(3, 3)}`
     document.body.appendChild(element);
 });
